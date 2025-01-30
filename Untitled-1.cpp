5d8a7887c69c3bf9a78289a5d025034c8a2e06be
Bao Chau <ngovietbaochau2005@gmail.com>
2025-01-30 11:22:32 +0700

    add Hello.c file

---
+++ Untitled-1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+#include <cmath>
+
+int main(){
+    float a;
+    cout << "Nhap a: ";
+    cin >> a;;
+    cout << "Can bac 2 cua a la: " << sqrt(a);
+    cout << "\na^5 la: " << pow(a, 5);
+    return 0;
+}
